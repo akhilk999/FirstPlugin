64b1efa90a4c654a23fc5d620241ddf646cc0627
Akhil Kasamsetty <awesomesetty99@gmail.com>
2023-05-28 18:41:15 -0500

    first commit

---
+++ src/main/java/org/akhilk999/firstplugin/FirstPlugin.java
@@ -0,0 +1,17 @@
+package org.akhilk999.firstplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FirstPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
